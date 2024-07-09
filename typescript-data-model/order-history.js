23a4099c00920c417f35332665f974b70e9a3e01
Tristan Chao <chaot372@gmail.com>
2024-07-09 19:39:07 +0000

    updated typescript-data-model solution

--- typescript-data-model/order-history.js
+++ typescript-data-model/order-history.js
@@ -1,63 +1,78 @@
 'use strict';
 const orderHistory = [
   {
-    itemNames: ['JavaScript for impatient programmers'],
+    items: [
+      {
+        name: 'JavaScript for impatient programmers',
+        price: 31.55,
+        bookAuthor: 'Rauschmayer, Dr. Axel',
+      },
+    ],
     numberOfItems: 1,
     orderPlaced: 'Aug 4, 2020',
     delivered: true,
     deliveryDate: 'Aug 8, 2020',
     shipTo: 'JS Masher',
-    itemPrices: [31.55],
     orderTotal: 34,
     orderNumber: '114-3941689-8772232',
     returnWindowEnd: 'Sep 7, 2020',
     returnWindowClosed: true,
-    bookAuthors: ['Rauschmayer, Dr. Axel'],
   },
   {
-    itemNames: ['The Timeless Way of Building'],
+    items: [
+      {
+        name: 'The Timeless Way of Building',
+        price: 41.33,
+        bookAuthor: 'Alexander, Christopher',
+      },
+    ],
     numberOfItems: 1,
     orderPlaced: 'July 19, 2020',
     delivered: true,
     deliveryDate: 'July 20, 2020',
     shipTo: 'JS Masher',
-    itemPrices: [41.33],
     orderTotal: 44.53,
     orderNumber: '113-9984268-1280257',
     returnWindowEnd: 'Aug 19, 2020',
     returnWindowClosed: true,
-    bookAuthors: ['Alexander, Christopher'],
   },
   {
-    itemNames: [
-      'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    items: [
+      {
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        price: 15.98,
+      },
     ],
     numberOfItems: 1,
     orderPlaced: 'July 4, 2020',
     delivered: true,
     deliveryDate: 'July 7, 2020',
     shipTo: 'JS Masher',
-    itemPrices: [15.98],
     orderTotal: 17.22,
     orderNumber: '114-2875558-9059409',
     returnWindowEnd: 'Aug 5, 2020',
     returnWindowClosed: true,
   },
   {
-    itemNames: [
-      'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-      'The Art of Sql',
+    items: [
+      {
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        price: 94.95,
+      },
+      {
+        name: 'The Art of Sql',
+        price: 33.99,
+        bookAuthor: 'Faroult, Stephane',
+      },
     ],
     numberOfItems: 2,
     orderPlaced: 'July 3, 2020',
     delivered: true,
     deliveryDate: 'July 5, 2020',
     shipTo: 'JS Masher',
-    itemPrices: [94.95, 33.99],
     orderTotal: 138.93,
     orderNumber: '113-2883177-2648248',
     returnWindowEnd: 'Aug 4, 2020',
     returnWindowClosed: true,
-    bookAuthors: ['n/a', 'Faroult, Stephane'],
   },
 ];
--- typescript-data-model/order-history.ts
+++ typescript-data-model/order-history.ts
@@ -1,59 +1,73 @@
+interface Item {
+  name: string;
+  price: number;
+  bookAuthor?: string;
+}
+
 interface Order {
-  itemNames: string[];
+  items: Item[];
   numberOfItems: number;
   orderPlaced: string;
   delivered: boolean;
   deliveryDate?: string;
   shipTo: string;
-  itemPrices: number[];
   orderTotal: number;
   orderNumber: string;
   returnWindowEnd: string;
   returnWindowClosed: boolean;
-  bookAuthors?: string[];
 }
 
 const orderHistory: Order[] = [
   {
-    itemNames: ['JavaScript for impatient programmers'],
+    items: [
+      {
+        name: 'JavaScript for impatient programmers',
+        price: 31.55,
+        bookAuthor: 'Rauschmayer, Dr. Axel',
+      },
+    ],
     numberOfItems: 1,
     orderPlaced: 'Aug 4, 2020',
     delivered: true,
     deliveryDate: 'Aug 8, 2020',
     shipTo: 'JS Masher',
-    itemPrices: [31.55],
     orderTotal: 34,
     orderNumber: '114-3941689-8772232',
     returnWindowEnd: 'Sep 7, 2020',
     returnWindowClosed: true,
-    bookAuthors: ['Rauschmayer, Dr. Axel'],
   },
 
   {
-    itemNames: ['The Timeless Way of Building'],
+    items: [
+      {
+        name: 'The Timeless Way of Building',
+        price: 41.33,
+        bookAuthor: 'Alexander, Christopher',
+      },
+    ],
     numberOfItems: 1,
     orderPlaced: 'July 19, 2020',
     delivered: true,
     deliveryDate: 'July 20, 2020',
     shipTo: 'JS Masher',
-    itemPrices: [41.33],
     orderTotal: 44.53,
     orderNumber: '113-9984268-1280257',
     returnWindowEnd: 'Aug 19, 2020',
     returnWindowClosed: true,
-    bookAuthors: ['Alexander, Christopher'],
   },
 
   {
-    itemNames: [
-      'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    items: [
+      {
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        price: 15.98,
+      },
     ],
     numberOfItems: 1,
     orderPlaced: 'July 4, 2020',
     delivered: true,
     deliveryDate: 'July 7, 2020',
     shipTo: 'JS Masher',
-    itemPrices: [15.98],
     orderTotal: 17.22,
     orderNumber: '114-2875558-9059409',
     returnWindowEnd: 'Aug 5, 2020',
@@ -61,20 +75,25 @@ const orderHistory: Order[] = [
   },
 
   {
-    itemNames: [
-      'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-      'The Art of Sql',
+    items: [
+      {
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        price: 94.95,
+      },
+      {
+        name: 'The Art of Sql',
+        price: 33.99,
+        bookAuthor: 'Faroult, Stephane',
+      },
     ],
     numberOfItems: 2,
     orderPlaced: 'July 3, 2020',
     delivered: true,
     deliveryDate: 'July 5, 2020',
     shipTo: 'JS Masher',
-    itemPrices: [94.95, 33.99],
     orderTotal: 138.93,
     orderNumber: '113-2883177-2648248',
     returnWindowEnd: 'Aug 4, 2020',
     returnWindowClosed: true,
-    bookAuthors: ['n/a', 'Faroult, Stephane'],
   },
 ];
