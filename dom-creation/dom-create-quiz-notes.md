cef3675440e06fc93bf231efd9f80914549443bf
Tristan Chao <chaot372@gmail.com>
2024-07-22 23:18:29 +0000

    completed dom-creation exercise

---
+++ dom-creation/dom-create-quiz-notes.md
@@ -0,0 +1,49 @@
+# dom-creation-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- Does the `document.createElement()` method insert a new element into the page?
+  - no
+- How do you add an element as a child to another element?
+  - with the appendChild method
+- What do you pass as the arguments to the `element.setAttribute()` method?
+  - the attribute name as a string and the value as a string
+- What steps do you need to take in order to insert a new element into the page?
+  - you need to create the element and insert it in the DOM
+- What is the `textContent` property of an element object for?
+  - it is a getter/setter that can either retrieve the current text content in an element or be used to assign new text content to an element
+- Name two ways to set the `class` attribute of a DOM element.
+  - by assigning a string to the className property
+  - by using the setAttribute method and calling it with the attribute name 'class' and the class names as a string
+- What are two advantages of defining a function to do create something (like the work of creating a DOM tree)?
+  - you can make the function to be reusable, allowing you to save time and space by using one function to repeat similar tasks
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ dom-creation/main.ts
@@ -0,0 +1,111 @@
+interface Pokemon {
+  number: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+}
+
+const pokedex: Pokemon[] = [
+  {
+    number: '001',
+    name: 'Bulbasaur',
+    description:
+      'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+    imageUrl: 'images/bulbasaur.png',
+  },
+  {
+    number: '004',
+    name: 'Charmander',
+    description:
+      'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+    imageUrl: 'images/charmander.png',
+  },
+  {
+    number: '007',
+    name: 'Squirtle',
+    description:
+      'When it retracts its long neck into its shell, it squirts out water with vigorous force.',
+    imageUrl: 'images/squirtle.png',
+  },
+  {
+    number: '002',
+    name: 'Ivysaur',
+    description:
+      'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
+    imageUrl: 'images/ivysaur.png',
+  },
+  {
+    number: '005',
+    name: 'Charmeleon',
+    description:
+      'It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.',
+    imageUrl: 'images/charmeleon.png',
+  },
+  {
+    number: '008',
+    name: 'Wartortle',
+    description:
+      'It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.',
+    imageUrl: 'images/wartortle.png',
+  },
+  {
+    number: '003',
+    name: 'Venusaur',
+    description:
+      'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+    imageUrl: 'images/venusaur.png',
+  },
+  {
+    number: '006',
+    name: 'Charizard',
+    description:
+      'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
+    imageUrl: 'images/charizard.png',
+  },
+  {
+    number: '009',
+    name: 'Blastoise',
+    description:
+      'It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.',
+    imageUrl: 'images/blastoise.png',
+  },
+];
+
+function renderPokemon(pokeObj: Pokemon): HTMLDivElement {
+  const $column = document.createElement('div');
+  $column.className = 'column-third';
+
+  const $card = document.createElement('div');
+  $card.className = 'pokemon-card';
+  $column.appendChild($card);
+
+  const $img = document.createElement('img');
+  $img.setAttribute('src', pokeObj.imageUrl);
+  $card.appendChild($img);
+
+  const $cardText = document.createElement('div');
+  $cardText.className = 'pokemon-card-text';
+  $card.appendChild($cardText);
+
+  const $name = document.createElement('h2');
+  $name.textContent = pokeObj.name;
+  $cardText.appendChild($name);
+
+  const $number = document.createElement('h3');
+  $number.textContent = '#' + pokeObj.number;
+  $cardText.appendChild($number);
+
+  const $descr = document.createElement('p');
+  $descr.textContent = pokeObj.description;
+  $cardText.appendChild($descr);
+
+  return $column;
+}
+
+const $row = document.querySelector('.row');
+if (!$row) throw new Error('$row query failed');
+
+for (let i = 0; i < pokedex.length; i++) {
+  const $newPokeCard = renderPokemon(pokedex[i]);
+  $row.appendChild($newPokeCard);
+}
