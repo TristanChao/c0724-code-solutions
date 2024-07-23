a20eb6188810f7bcb606f3f0f1a744a425167490
Tristan Chao <chaot372@gmail.com>
2024-07-23 21:04:03 +0000

    completed typescript-light-bulb exercise

---
+++ typescript-light-bulb/main.js
@@ -0,0 +1,14 @@
+'use strict';
+const $lightBulb = document.querySelector('.bulb');
+const $background = document.querySelector('.back');
+if (!$lightBulb) throw new Error('$lightBulb query failed');
+if (!$background) throw new Error('$background query failed');
+$lightBulb.addEventListener('click', () => {
+  if ($lightBulb.className === 'bulb bulb-light') {
+    $lightBulb.className = 'bulb bulb-dark';
+    $background.className = 'back back-dark';
+  } else {
+    $lightBulb.className = 'bulb bulb-light';
+    $background.className = 'back back-light';
+  }
+});
