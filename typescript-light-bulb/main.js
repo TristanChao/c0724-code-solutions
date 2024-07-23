a4f79c6400264c4c4b4c489aff3edecadb2de9d4
Tristan Chao <chaot372@gmail.com>
2024-07-23 21:18:35 +0000

    modified  click event callback function

--- typescript-light-bulb/main.js
+++ typescript-light-bulb/main.js
@@ -3,8 +3,11 @@ const $lightBulb = document.querySelector('.bulb');
 const $background = document.querySelector('.back');
 if (!$lightBulb) throw new Error('$lightBulb query failed');
 if (!$background) throw new Error('$background query failed');
+let lightOn = true;
 $lightBulb.addEventListener('click', () => {
-  if ($lightBulb.className === 'bulb bulb-light') {
+  if (lightOn) lightOn = false;
+  else if (!lightOn) lightOn = true;
+  if (!lightOn) {
     $lightBulb.className = 'bulb bulb-dark';
     $background.className = 'back back-dark';
   } else {
--- typescript-light-bulb/main.ts
+++ typescript-light-bulb/main.ts
@@ -4,8 +4,13 @@ const $background = document.querySelector('.back');
 if (!$lightBulb) throw new Error('$lightBulb query failed');
 if (!$background) throw new Error('$background query failed');
 
+let lightOn = true;
+
 $lightBulb.addEventListener('click', () => {
-  if ($lightBulb.className === 'bulb bulb-light') {
+  if (lightOn) lightOn = false;
+  else if (!lightOn) lightOn = true;
+
+  if (!lightOn) {
     $lightBulb.className = 'bulb bulb-dark';
     $background.className = 'back back-dark';
   } else {
