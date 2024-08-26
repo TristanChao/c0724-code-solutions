47b95402671abf9e6d017fdb3e31a8772ad6fb6a
Tristan Chao <chaot372@gmail.com>
2024-08-26 20:44:33 +0000

    completed react-jsx exercise

---
+++ react-jsx/react-jsx-app/src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '../../../../../vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React JSX</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Total Button Clicks: {count}
+        </button>
+        <p>
+          JSX is a way of creating DOM elements in a way that closely resembles
+          HTML format, making it quicker to write and easier to read.
+        </p>
+        <p>
+          React uses JSX to render components by converting it to HTML, then
+          pushing it to the DOM.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
